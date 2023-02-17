08239a9f808d073a05c8cf059120a22063b5e442
rachid ennajjar <rachidennajjar7@gmail.com>
2023-02-17 11:18:06 -0500

    Kchecks1PY

--- KChecks1.py
+++ KChecks1.py
@@ -5,7 +5,7 @@ Cars = ['Toyota', 'Chevy', 'Dodge', 'BMW']
 print(Cars[1])
 
 #Dictionary populated with two keys and two values
-Car_prices =  {'Toyota' : '210000', 'Chevy' : '190000'}
+Car_prices =  {'Toyota': '210000', 'Chevy': '190000'}
 print(Car_prices['Toyota'])
 
 #Tuple with 4 values
