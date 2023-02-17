c750bdebfd24ac09e3f86b9f52138d97529602cc
rachid ennajjar <rachidennajjar7@gmail.com>
2023-02-17 11:09:29 -0500

    new file: KChecks1.py

---
+++ KChecks1.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+#List populated with several values
+Cars = ['Toyota', 'Chevy', 'Dodge', 'BMW']
+print(Cars[1])
+
+#Dictionary populated with two keys and two values
+Car_prices =  {'Toyota' : '210000', 'Chevy' : '190000'}
+print(Car_prices['Toyota'])
+
+#Tuple with 4 values
+Car_colors = ('green', 'blue', 'grey', 'white')
+print(Car_colors[1])
